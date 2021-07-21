81d35d5775e7e389411bcf48ede4d6812d2282ea
Jason Trinity <kayceeanosike1@gmail.com>
2021-07-21 12:03:39 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pie Checker 
+
+A C++ program that can check the amount of apples needed to make a pie.
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, pie, apples;
+    cout << "enter the number of fruits in the bowl" << endl;
+    cin>> n;
+    apples= n/2;
+    pie= apples/3;
+    cout<<"the number of pies that can be made = "<<pie<<endl;
+    return 0;
+}
